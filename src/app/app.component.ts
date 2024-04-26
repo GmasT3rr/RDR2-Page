28f529271e7c962652d8dbda671eb7292c1cfbfd
Lucas Ramallo <lucramallo14@gmail.com>
2024-04-26 13:41:23 -0300

    changed images path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,48 +48,48 @@ export class AppComponent {
       class: 'arthur',
       description:
         'The stalwart lieutenant of the gang, epitomizes loyalty and honor.His stoic demeanor masks a heart of gold, as he fiercely protectshis family and comrades. Arthur´s moral compass guides him throughlife´s challenges, as he grapples with questions of morality andloyalty amidst a changing world.',
-      imageSrc: '../assets/images/arthur.png',
+      imageSrc: 'assets/images/arthur.png',
     },
     {
       name: 'John Marston',
       class: 'john',
       description:
         'A former outlaw seeking redemption, possesses a rugged yet introspective demeanor. His past experiences have instilled in him asense of duty and determination to right his wrongs. Despite hisrough exterior, John harbors a deep sense of loyalty and compassiontowards those he cares about.',
-      imageSrc: '../assets/images/marston.png',
+      imageSrc: 'assets/images/marston.png',
     },
     {
       name: 'Dutch Vanderlinde',
       class: 'dutch',
       description:
         'The magnetic leader of the gang, exudes charm and charisma. His captivating speeches and idealistic vision inspire unwaveringloyalty among his followers. However, beneath his charismatic facadelies a man willing to push moral boundaries in pursuit of hisutopian ideals.',
-      imageSrc: '../assets/images/dutch.png',
+      imageSrc: 'assets/images/dutch.png',
     },
   ];
 
   itemList = [
     {
       title: 'Story',
-      imageSrc: '../assets/images/story.jpg',
+      imageSrc: 'assets/images/story.jpg',
     },
     {
       title: 'Characters',
-      imageSrc: '../assets/images/characters.jpg',
+      imageSrc: 'assets/images/characters.jpg',
     },
     {
       title: 'Gameplay',
-      imageSrc: '../assets/images/gameplay.jpg',
+      imageSrc: 'assets/images/gameplay.jpg',
     },
     {
       title: 'Weaponry',
-      imageSrc: '../assets/images/weaponry.jpg',
+      imageSrc: 'assets/images/weaponry.jpg',
     },
     {
       title: 'Wildlife',
-      imageSrc: '../assets/images/wildlife.jpg',
+      imageSrc: 'assets/images/wildlife.jpg',
     },
     {
       title: 'Soundtrack',
-      imageSrc: '../assets/images/soundtrack.jpg',
+      imageSrc: 'assets/images/soundtrack.jpg',
     },
   ];
 }
